dade577b0cd16d57667e99d3d403ccc27d5d8822
Yann <yannpietroc@gmail.com>
2025-06-23 21:19:46 -0300

    9- Programando a passagem de slides

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@ const hamburguer = document.getElementById("hamburguer");
 const links = document.querySelector(".menu-links");
 const header = document.querySelector("header");
 const banner = document.querySelector(".inicio");
+const mudarLanche = document.querySelectorAll(".mudar-lanche");
+const slides = document.querySelectorAll(".slide");
+let slide = 1;
 
 window.addEventListener("scroll", () => { //Função para mudar estilo do header ao scrollar a página
     if(window.scrollY > 150) {
@@ -29,3 +32,36 @@ window.addEventListener("load", () => { //Diversas fotos de fundo
     const currentBanner = Math.floor(Math.random() * 3) + 1;
     banner.style.backgroundImage = `var(--gradient), url(./src/assets/banner${currentBanner}.jpg)`;
 });
+
+mudarLanche[0].addEventListener("click", () => mudarSlide("-"));
+mudarLanche[1].addEventListener("click", () => mudarSlide("+"));
+function mudarSlide(index) { //Mudar slide de lanche
+    switch(index) {
+        case "+":
+            slide++;
+            if(slide == 4) {
+                slide = 1;
+            }
+            break;
+        case "-":
+            slide--;
+            if(slide == 0) {
+                slide = 3;
+            }
+            break;
+    }
+    slides.forEach(e => {
+        e.classList.remove("active", "left", "right");
+    });
+    switch(slide) {
+        case 1:
+            slides[0].classList.add("active");
+            break;
+        case 2:
+            slides[1].classList.add("active");
+            break;
+        case 3:
+            slides[2].classList.add("active");
+            break;
+    }
+}
